6ff418e0e07c9a850931180e32806df918c2a42f
AnhPT <anhpt@miichisoft.net>
2025-04-14 10:07:56 +0700

    ối dồi ôi

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,14 +1,34 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { hashSync } from 'bcrypt';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { QueryError } from 'mysql2';
+import { join } from 'path';
+import PdfPrinter from 'pdfmake';
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
 import { PaginatedDto, PaginationQuery } from '~/common/dto';
 import { ErrorCode, Ix, SqlError } from '~/common/enums';
+import { User } from '~/entities';
 import { UserRepository } from '~/repositories';
-import { CreateUserDto, UpdateUserDto } from './user.dto';
+import { UserDto, UserQuery } from './user.dto';
 
 @Injectable()
 export class UserService {
-  constructor(private userRepository: UserRepository) {}
+  private fontsDirPath = join(process.cwd(), 'fonts');
+
+  private printer: PdfPrinter;
+
+  constructor(private userRepository: UserRepository) {
+    this.printer = new PdfPrinter({
+      Roboto: {
+        normal: join(this.fontsDirPath, 'Roboto-Regular.ttf'),
+        bold: join(this.fontsDirPath, 'Roboto-Medium.ttf'),
+        italics: join(this.fontsDirPath, 'Roboto-Italic.ttf'),
+        bolditalics: join(this.fontsDirPath, 'Roboto-MediumItalic.ttf'),
+      },
+    });
+  }
 
   async paginate({
     limit,
@@ -19,7 +39,7 @@ export class UserService {
   }: PaginationQuery) {
     const qb = this.userRepository.createQueryBuilder('user');
     if (keyword) {
-      qb.andWhere('(user.username LIKE :keyword OR user.email LIKE :keyword)', {
+      qb.andWhere('user.username LIKE :keyword OR user.email LIKE :keyword', {
         keyword: `%${keyword}%`,
       });
     }
@@ -29,40 +49,62 @@ export class UserService {
     if (status) {
       qb.andWhere('user.status = :status', { status });
     }
-    const [users, count] = await qb
-      .orderBy(sort.by, sort.direction)
+    qb.orderBy(sort.by, sort.direction)
       .take(limit)
-      .skip(limit * (page - 1))
-      .getManyAndCount();
-    return new PaginatedDto({ results: users, total: count });
+      .skip(limit * (page - 1));
+    const [users, total] = await qb.getManyAndCount();
+    return new PaginatedDto({ results: users, total });
+  }
+
+  async getRoles({ keyword }: UserQuery) {
+    const qb = this.userRepository
+      .createQueryBuilder('user')
+      .select('DISTINCT user.role as role');
+    if (keyword) {
+      qb.andWhere('user.role LIKE :keyword', { keyword: `%${keyword}%` });
+    }
+    const users: User[] = await qb.getRawMany();
+    return users.map((user) => user.role);
+  }
+
+  async getStatuses({ keyword }: UserQuery) {
+    const qb = this.userRepository
+      .createQueryBuilder('user')
+      .select('user.status AS status');
+    if (keyword) {
+      qb.andWhere('user.status LIKE :keyword', { keyword: `%${keyword}%` });
+    }
+    qb.groupBy('user.status');
+    const users: User[] = await qb.getRawMany();
+    return users.map((user) => user.status);
+  }
+
+  generateFile() {
+    const docDefinition: TDocumentDefinitions = {
+      content: [
+        'First paragraph',
+        'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines',
+      ],
+    };
+    return this.printer.createPdfKitDocument(docDefinition);
   }
 
   getById(id: string) {
     return this.userRepository.findOne({ where: { id } });
   }
 
-  async create({ password, ...rest }: CreateUserDto) {
+  async create(dto: UserDto) {
     try {
-      await this.userRepository.insert({
-        ...rest,
-        hashedPassword: hashSync(password, 10),
-      });
+      await this.userRepository.insert(dto);
     } catch (error) {
       this.handleSqlError(error);
     }
   }
 
-  async update(id: string, { password, ...rest }: UpdateUserDto) {
+  async update(id: string, dto: UserDto) {
     const user = await this.userRepository.findOne({ where: { id } });
     try {
-      await this.userRepository.update(
-        { id },
-        {
-          ...user,
-          ...rest,
-          hashedPassword: password && hashSync(password, 10),
-        },
-      );
+      await this.userRepository.update({ id }, { ...user, ...dto });
     } catch (error) {
       this.handleSqlError(error);
     }
@@ -83,6 +125,6 @@ export class UserService {
         throw new BadRequestException(ErrorCode.EMAIL_ALREADY_EXISTS);
       }
     }
-    throw error;
+    throw new InternalServerErrorException(error);
   }
 }
